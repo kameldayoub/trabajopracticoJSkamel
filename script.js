9d79c861afd13298206002291ee9758a68f6dc1e
Kamel Dayoub <kamel.dayoub.epet20@gmail.com>
2022-10-05 12:54:57 -0300

    trabajo JS

---
+++ script.js
@@ -0,0 +1,52 @@
+//#region 
+var nombre1 = "Kamel";
+const nombre2 = 'Nur';
+let apellido = `Dayoub`;
+var inicio=0;
+var fin=[3];
+//#endregion
+
+//#region 
+console.log(nombre1);
+console.log(nombre2);
+console.log(apellido);
+//#endregion
+
+
+//#region 
+var suma = (num1,num2) =>{
+    
+    console.log(num1+ num2);
+}
+//#endregion
+
+//#region 
+var resta = (num1,num2) =>{
+    
+    console.log(num1- num2);
+}
+//#endregion
+
+//#region 
+var multiplicacion = (num1,num2) =>{
+    
+    console.log(num1* num2);
+}
+//#endregion
+
+//#region 
+var division = (num1,num2) =>{
+    
+    console.log(num1/ num2);
+}
+//#endregion
+
+//#region 
+let persona=[nombre1,nombre2,apellido];
+//#endregion
+
+//#region 
+for (inicion of fin){
+    console.log(persona[inicio]);
+}
+//#endregion
